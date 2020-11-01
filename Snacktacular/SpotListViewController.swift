23a344a2782779bd9a34d9c15c5ee9807f85b231
Young Hur <huryo@bc.edu>
2020-11-01 17:13:48 -0500

    Finished 8.4 - Color Assets, Navigation + Toolbar Colors, Status Bar Color, Buttons with Images, and Customizing Segmented Controls

--- Snacktacular/SpotListViewController.swift
+++ Snacktacular/SpotListViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class SpotListViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var sortSegmentedControl: UISegmentedControl!
     
     var spots = ["Island Creek Oysters", "El Pelon", "Shake Shack", "Pino's Pizza"]
 
@@ -16,10 +17,22 @@ class SpotListViewController: UIViewController {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-
+        configureSegmentedControl()
+    }
+    
+    func configureSegmentedControl() {
+        // Set font colors for segmented control
+        let orangeFontColor = [NSAttributedString.Key.foregroundColor : UIColor(named: "PrimaryColor") ?? UIColor.orange]
+        let whiteFontColor = [NSAttributedString.Key.foregroundColor : UIColor.white]
+        sortSegmentedControl.setTitleTextAttributes(orangeFontColor, for: .selected)
+        sortSegmentedControl.setTitleTextAttributes(whiteFontColor, for: .normal)
+        // Add white border to segmented control
+        sortSegmentedControl.layer.borderColor = UIColor.white.cgColor
+        sortSegmentedControl.layer.borderWidth = 1.0
     }
 
 }
+
 extension SpotListViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return spots.count
