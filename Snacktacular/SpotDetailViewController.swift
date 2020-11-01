a10ebce7f33ccbc90716d2e7c6cc20574c5f3f34
Young Hur <huryo@bc.edu>
2020-11-01 16:48:29 -0500

    Finished 8.3 - Setting up the Initial Snacktacular UI

---
+++ Snacktacular/SpotDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SpotDetailViewController.swift
+//  Snacktacular
+//
+//  Created by Young Hur on 11/1/20.
+//
+
+import UIKit
+
+class SpotDetailViewController: UIViewController {
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var ratingLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        let isPresentingInAddMode = presentingViewController is UINavigationController
+        if isPresentingInAddMode {
+            dismiss(animated: true, completion: nil)
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+}
---
+++ Snacktacular/SpotTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SpotTableViewCell.swift
+//  Snacktacular
+//
+//  Created by Young Hur on 11/1/20.
+//
+
+import UIKit
+
+class SpotTableViewCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    
+
+
+}
