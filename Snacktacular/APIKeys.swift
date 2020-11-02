f346a8149f563975f15c0af06e4f9b5384e9eb2e
Young Hur <huryo@bc.edu>
2020-11-01 20:22:00 -0500

    Finished 8.7 - Add Google Place Autocomplete

---
+++ Snacktacular/APIKeys.swift
@@ -0,0 +1,12 @@
+//
+//  APIKeys.swift
+//  Snacktacular
+//
+//  Created by Young Hur on 11/1/20.
+//
+
+import Foundation
+
+struct APIKeys {
+    static let googlePlacesKey = "AIzaSyABXZLp3h1vEzV4EMa2QUu8-6Gi7bCl3Lc"
+}
