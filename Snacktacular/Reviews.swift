f3ac88ea9a16333e47a43194eb2182a8fdb4f08b
Young Hur <huryo@bc.edu>
2020-11-07 15:34:35 -0500

    Finished 8.15 - The Reviews Class and Loading a Collection in a Cloud Firestore Document

--- Snacktacular/Reviews.swift
+++ Snacktacular/Reviews.swift
@@ -16,4 +16,25 @@ class Reviews {
     init() {
         db = Firestore.firestore()
     }
+    
+    func loadData(spot: Spot, completed: @escaping () -> ()) {
+        guard spot.documentID != "" else {
+            return
+        }
+        db.collection("spots").document(spot.documentID).collection("reviews").addSnapshotListener { (querySnapshot, error) in
+            guard error == nil else {
+                print("😡 ERROR: adding the snapshot listener \(error!.localizedDescription)")
+                return completed()
+            }
+            self.reviewArray = [] // Clean out existing spotArray since new data will load
+            // There are querySnapshot!.documents.count documents in the snapshot
+            for document in querySnapshot!.documents {
+                // You'll have to make sure you have an initializer in the singular class
+                let review = Review(dictionary: document.data())
+                review.documentID = document.documentID
+                self.reviewArray.append(review)
+            }
+            completed()
+        }
+    }
 }
