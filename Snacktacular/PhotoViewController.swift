24285d85cfb96ea2afc4cfd6b326f85b6e50b171
Young Hur <huryo@bc.edu>
2020-11-15 15:01:50 -0500

    Finished 8.24 - Smooth Firestore Storage Image Loading w/SDWebImage

--- Snacktacular/PhotoViewController.swift
+++ Snacktacular/PhotoViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import SDWebImage
 
 private let dateFormatter: DateFormatter = {
     let dateFormatter = DateFormatter()
@@ -68,6 +69,14 @@ class PhotoViewController: UIViewController {
                 descriptionTextView.backgroundColor = .white
             }
         }
+        guard let url = URL(string: photo.photoURL) else {
+            // Then this must be a new image, so get the image from the photo.image passed in rather than from the url
+            photoImageView.image = photo.image
+            return
+        }
+        photoImageView.sd_imageTransition = .fade
+        photoImageView.sd_imageTransition?.duration = 0.5
+        photoImageView.sd_setImage(with: url)
     }
     
     func updateFromUserInterface() {
