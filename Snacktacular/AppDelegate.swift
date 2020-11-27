255fe09dbcd6856edb5927d1490bb1450f50dab9
Young Hur <huryo@bc.edu>
2020-11-27 16:38:05 -0500

    Finished 8.29 - Finished Snacktacular

--- Snacktacular/AppDelegate.swift
+++ Snacktacular/AppDelegate.swift
@@ -13,7 +13,6 @@ import GooglePlaces
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         GMSPlacesClient.provideAPIKey(APIKeys.googlePlacesKey)
